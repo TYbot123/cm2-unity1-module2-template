9307a82a081bc28c790938807704ec5f3b6c121a
TYbot123 <derickwang0907@gmail.com>
2026-02-14 17:21:20 -0800

    catch ai
    moving to the player

---
+++ Assets/toplayer.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class toplayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    private UnityEngine.AI.NavMeshAgent agent;
+   
+    void Start()
+    {
+      agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(target != null)
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
